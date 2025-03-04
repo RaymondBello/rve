95ded93e3bc3af6fbc0b5ebed4278b0011273a92
Ray <61327086+RaymondBello@users.noreply.github.com>
2025-03-04 10:21:58 -0500

    Adding files

---
+++ rv32ima/main.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+// #include "app.h"
+
+
+// Main code
+int main(int, char**)
+{
+    printf("Hello world\n");
+    // App app;
+    // // Init
+    // app.initializeWindow();
+    // app.initializeUI();
+    // // Running
+    // app.renderLoop();
+    // // Close
+    // app.destroyUI();
+}
