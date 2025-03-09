53e22b54e33c6e74e0a0f41bfe813a3e8ac26246
RaymondBello <raymondbello11@gmail.com>
2025-03-08 21:23:58 -0500

    clean-up

--- rv32ima/main.cpp
+++ rv32ima/main.cpp
@@ -1,17 +1,8 @@
 #include <stdio.h>
-// #include "app.h"
-
 
 // Main code
 int main(int, char**)
 {
     printf("Hello world\n");
-    // App app;
-    // // Init
-    // app.initializeWindow();
-    // app.initializeUI();
-    // // Running
-    // app.renderLoop();
-    // // Close
-    // app.destroyUI();
+    return 0;
 }
